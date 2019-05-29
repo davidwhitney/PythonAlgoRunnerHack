92ce9e3d68a90d0bf41aab5292c19367df519619
David Whitney <david.whitney@gfk.com>
2019-05-29 17:18:18 +0100

    Formatting bits.

--- AlgoRuntime/temp_test_algo/verify.py
+++ AlgoRuntime/temp_test_algo/verify.py
@@ -3,11 +3,12 @@ qpic = importlib.import_module('sdk.assert', '../sdk/assert.py')
 
 def verify(results):
     qpic.check(
-        "Should have four arguments", 
+        "Should have four arguments",
         lambda: results["__qpis"]["number_of_args"] == 4, 
-        "FATAL")
+        "FATAL"
+    )
 
     for cell in results["dataframe1"]:
-        if cell.something > 1: qpic.warn("Oh no, greater than 1!", cell)
+        if cell["something"] > 1: qpic.warn("Oh no, greater than 1!", cell)
 
     return qpic.results
